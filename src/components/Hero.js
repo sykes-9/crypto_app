1e1fdaac3d676da67e0e17681432c1f794e4d492
sykes-9 <sykessackitey9@gmail.com>
2023-07-07 14:50:53 +0000

    react crypto web app pulling using usestate to pull info from coin gecko free api

---
+++ src/components/Hero.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Hero.css'
+import Crypto from '../assets/hero-img.png'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='container'>
+
+            {/* left side */}
+            <div className="left">
+                <p>Buy & Sell Crypto 24/7 using your retirement account</p>
+                <h1>Invest in Cryptocurrency with Your IRA</h1>
+                <p>Buy, Sell and store hundreds of Cryptocurrencies</p>
+                <div className="input-container">
+                    <input type="email" placeholder='Enter your email' />
+                    <button className="btn">Learn More</button>
+                </div>
+            </div>
+
+
+            {/* right side */}
+            <div className="right">
+                <div className='img-container'>
+                    <img src={Crypto} alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
